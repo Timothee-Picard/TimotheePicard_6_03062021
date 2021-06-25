2378ea367f8c32e7a37a2a3cd07d35670119fe8e
Timothee-Picard <timothee@atnos.com>
2021-06-26 00:49:32 +0200

    feat: add system filters and rewrite code with class

--- js/app.js
+++ js/app.js
@@ -4,45 +4,112 @@ const id = urlParams.get('id')
 const path = window.location.pathname;
 
 var main = document.querySelector("main")
+var nav = document.querySelector("nav")
 
 const router = {
     home: "/index.html",
     photographerPage: "/views/photographer-page.html"
 }
 
+const Photograph = class {
+    
+    constructor(id, name, tagline, country, city, portrait, price, tags) {
+        this.id = id
+        this.name = name
+        this.tagline = tagline
+        this.country = country
+        this.city = city
+        this.portrait = portrait
+        this.price = price
+        this.tags = tags
+    }
+
+    displayPhotograph() {
+        let tagsHTML = ""
+            this.tags.forEach(tag => {
+                tagsHTML += '<span>#' + tag + '</span>'
+            })
+        let article = `
+            <article>
+                <header>
+                    <img src="${"./assets/Photographers ID Photos/" + this.portrait}" class="presentationImg">
+                    <h2>
+                        <a href="${"./views/photographer-page.html?id=" + this.id}">${this.name}</a>
+                    </h2>
+                </header>
+                <address>${this.city + ", " + this.country}</address>
+                <p>${this.tagline}</p>
+                <p class="price">${this.price + '€/jour'}/jour</p>
+                <footer>
+                    ${tagsHTML}
+                </footer>
+            </article>`
+
+        return article
+    }
+}
+
+const PhotographList = class {
+    constructor(datas) {
+        this.photographs = []
+        this.sortPhotographs = []
+        datas.forEach(data => {
+            this.photographs.push(new Photograph(data.id, data.name, data.tagline, data.country, data.city, data.portrait, data.price, data.tags))
+        })
+    }
+
+    sortList(sort) {
+        this.sortPhotographs = []
+        if(sort === undefined){
+            this.sortPhotographs = this.photographs
+        }
+        else {
+            this.photographs.forEach(photograph => {
+                photograph.tags.forEach(tag => {
+                    if(sort == tag) {
+                        this.sortPhotographs.push(photograph)
+                    }
+                })
+            })
+        }
+    }
+
+    displayList() {
+        let content = ""
+        this.sortPhotographs.forEach(sortPhotograph => {
+            content += sortPhotograph.displayPhotograph()
+        })
+        main.innerHTML = content
+    }
+
+    displayFilters() {
+        let sorts = []
+        this.photographs.forEach(photographer => {
+            photographer.tags.forEach(tag => {
+                sorts.push(tag)
+            })
+        })
+        let filters = [...new Set(sorts)]
+        filters.forEach(filter => {
+            let span = document.createElement("span")
+            span.textContent = `#${filter}`
+            nav.appendChild(span)
+            span.addEventListener("click", achanger =>{
+                this.sortList(filter)
+                this.displayList()
+            })
+        })
+    }
+}
+
 fetch('/assets/FishEyeData.json').then(response => { return response.json() })
 .then(data => {
     switch (path) {
         case router.home:
-            console.log("home")
-            let homeListHTML = ""
-            data.photographers.forEach(photographer => {
-
-                let photographerTagsHTML = ""
-
-                photographer.tags.forEach(tag => {
-                    photographerTagsHTML += '<span>#' + tag + '</span>'
-                })
-
-                homeListHTML += `
-                <article>
-                    <header>
-                        <img src="${"./assets/Photographers ID Photos/" + photographer.portrait}" class="presentationImg">
-                        <h2>
-                            <a href="${"./views/photographer-page.html?id=" + photographer.id}">${photographer.name}</a>
-                        </h2>
-                    </header>
-                    <address>${photographer.city + ", " + photographer.country}</address>
-                    <p>${photographer.tagline}</p>
-                    <p class="price">${photographer.price + '€/jour'}/jour</p>
-                    <footer>
-                        ${photographerTagsHTML}
-                    </footer>
-                </article>`
-            })
-
-            main.innerHTML = main.innerHTML + homeListHTML
-
+            var TestPhotographList = new PhotographList(data.photographers)
+            TestPhotographList.displayFilters()
+            TestPhotographList.sortList()
+            TestPhotographList.displayList()
             break
         case router.photographerPage:
             var photographe = {}
